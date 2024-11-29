d816e4fcce04d295fcbb6d87b7e99bd0b54ff0ad
FortyTwoCn <112876033+FortyTwoCn@users.noreply.github.com>
2024-11-29 17:13:38 +0800

    Create 星辉转化-block_transmutation.js

---
+++ kubejs/server_scripts/Examples/星辉魔法-Astralsorcery/星辉转化-block_transmutation.js
@@ -0,0 +1,21 @@
+//原版数据包格式
+{
+  "type": "astralsorcery:block_transmutation",
+  "input": [
+    {
+      "block": "minecraft:sea_lantern",
+      "display": {
+        "item": "minecraft:sea_lantern",
+        "count": 1
+      }
+    }
+  ],
+  "output": {
+    "block": "minecraft:lapis_block"
+  },
+  "display": {
+    "item": "minecraft:lapis_block",
+    "count": 1
+  },
+  "starlight": 200.0
+}
