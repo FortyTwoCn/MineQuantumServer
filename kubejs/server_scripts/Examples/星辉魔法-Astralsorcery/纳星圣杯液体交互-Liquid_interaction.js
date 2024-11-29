a4a9e2e85cd71406c477c34b4f1704167964a39f
FortyTwoCn <112876033+FortyTwoCn@users.noreply.github.com>
2024-11-29 18:04:44 +0800

    Update 纳星圣杯液体交互-Liquid_interaction.js

--- kubejs/server_scripts/Examples/星辉魔法-Astralsorcery/纳星圣杯液体交互-Liquid_interaction.js
+++ kubejs/server_scripts/Examples/星辉魔法-Astralsorcery/纳星圣杯液体交互-Liquid_interaction.js
@@ -1,3 +1,4 @@
+//10毫升和10毫升水相聚为一个原石
 //原版数据包
 {
   "type": "astralsorcery:liquid_interaction",
@@ -18,3 +19,25 @@
     }
   }
 }
+//使用kubejs
+onEvent('recipes', event => {
+  event.custom({
+    type: 'astralsorcery:liquid_interaction',
+    reactant1: 'minecraft:water',          // 流体一
+    reactant1Amount: 10,                   // 流体一合成一次的消耗量
+    reactant2: 'minecraft:lava',           // 流体二
+    reactant2Amount: 10,                   // 流体二合成一次的消耗量
+    chanceConsumeReactant1: 0.1,           // 10%概率消耗流体一
+    chanceConsumeReactant2: 0.1,           // 10%概率消耗流体二
+    weight: 3,                             // 相比于其他配方的权重
+    result: {
+      id: 'astralsorcery:drop_item',       // 掉落物
+      data: {
+        output: {
+          item: 'minecraft:stone',        // 物品ID
+          count: 1                        // 数量
+        }
+      }
+    }
+  })
+})
