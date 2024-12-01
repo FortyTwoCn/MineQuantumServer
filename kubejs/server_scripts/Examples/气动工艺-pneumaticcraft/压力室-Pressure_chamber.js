f5349a6dc37c92d8b6d34ca855109ab7535d0a3a
FortyTwoCn <112876033+FortyTwoCn@users.noreply.github.com>
2024-12-01 09:06:12 +0800

    Update 压力室-Pressure_chamber.js

--- kubejs/server_scripts/Examples/气动工艺-pneumaticcraft/压力室-Pressure_chamber.js
+++ kubejs/server_scripts/Examples/气动工艺-pneumaticcraft/压力室-Pressure_chamber.js
@@ -51,3 +51,31 @@ onEvent('recipes', event => {
     ]
   })
 })
+
+//多物品参考
+onEvent('recipes', event => {
+  event.custom({
+    type: 'pneumaticcraft:pressure_chamber',
+    inputs: [
+      {
+        item: 'minecraft:milk_bucket'  // 输入物品（牛奶桶）
+      },
+      {
+        type: 'pneumaticcraft:stacked_item',  // 输入类型为堆叠物品
+        tag: 'forge:dyes/green',              // 输入物品标签（绿色染料）
+        count: 4                              // 输入物品数量（4个绿色染料）
+      }
+    ],
+    pressure: 1.0,  // 所需压力（1.0 bar）
+    results: [
+      {
+        item: 'minecraft:slime_ball',  // 输出物品（史莱姆球）
+        count: 4                      // 输出数量（4个史莱姆球）
+      },
+      {
+        item: 'minecraft:bucket'      // 输出物品（桶）
+      }
+    ]
+  })
+})
+
