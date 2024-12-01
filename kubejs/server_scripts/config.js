5f889a690e4eb7635b1a8f4b98497b46644d14de
FortyTwoCn <112876033+FortyTwoCn@users.noreply.github.com>
2024-12-01 09:20:28 +0800

    Create config.js

---
+++ kubejs/server_scripts/config.js
@@ -0,0 +1,6 @@
+settings.logAddedRecipes = true
+settings.logRemovedRecipes = true
+settings.logSkippedRecipes = false
+settings.logErroringRecipes = true
+
+console.info('Hello, World! (每次重新加载服务器资源时，您都会看到此行消息)')
