332d4463854d3266b2b7c1d236e670d84fcc93c2
FortyTwoCn <112876033+FortyTwoCn@users.noreply.github.com>
2024-12-10 19:38:24 +0800

    Update Item.js

--- kubejs/server_scripts/Ban/Item.js
+++ kubejs/server_scripts/Ban/Item.js
@@ -1,6 +1,6 @@
 ServerEvents.recipes(event => {
     //Alex的生物Alexsmobs
-    event.remove({output:'alexsmobs:enderiophage_rocket'}), //移除末影噬菌体火箭
+    event.remove({output:'alexsmobs:enderiophage_rocket'}); // 移除末影噬菌体火箭
     //AE2
     event.remove({output: 'appliedenergistics2:spatial_pylon'}); // 移除空间塔
     event.remove({output: 'appliedenergistics2:spatial_io_port'}); // 移除空间io接口
@@ -18,5 +18,16 @@ ServerEvents.recipes(event => {
     event.remove({output: 'ae2wtlib:infinity_booster_card'}); // 移除无线增幅卡
     event.remove({output: 'appliedenergistics2:network_tool'}); // 移除网络工具
     // 时装工坊
-    event.remove({mod: 'armourers_workshop'})// 时装工坊mod
+    event.remove({mod: 'armourers_workshop'});// 时装工坊mod
+    // 无尽贪婪伪
+    event.remove({id: 'endless:sd_creative_storage_upgrade'}); // 创造抽屉升级
+    event.remove({id: 'endless:sd_creative_vending_upgrade'}); // 创造抽屉升级
+    // 星辉魔法
+    event.remove({id: 'astralsorcery:altar/knowledge_share'}); // 知识共享
+    event.remove({id: 'astralsorcery:altar/illumination_wand'}); // 辉光星杖
+    event.remove({id: 'astralsorcery:altar/exchange_wand'}); // 更替星杖
+    event.remove({id: 'astralsorcery:altar/illuminator'}); // 洞穴照明器
+    event.remove({id: 'astralsorcery:altar/fountain'}); // 万象泉
+    event.remove({id: 'astralsorcery:altar/fountain_prime_liquid'}); // 纳耳狂曼引口
+    event.remove({id: 'astralsorcery:altar/fountain_prime_vortex'}); // 费萨利德引口
 })
