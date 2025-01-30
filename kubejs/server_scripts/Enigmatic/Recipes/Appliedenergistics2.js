536fd082b8262afb74c718c620b34712a034ead3
FortyTwoCn <112876033+FortyTwoCn@users.noreply.github.com>
2025-01-30 18:03:32 +0800

    Create Appliedenergistics2.js

---
+++ kubejs/server_scripts/Enigmatic/Recipes/Appliedenergistics2.js
@@ -0,0 +1,20 @@
+//   ____                        _                     
+//  / __ \                      | |                    
+// | |  | | _   _   __ _  _ __  | |_  _   _  _ __ ___  
+// | |  | || | | | / _` || '_ \ | __|| | | || '_ ` _ \ 
+// | |__| || |_| || (_| || | | || |_ | |_| || | | | | |
+//  \___\_\ \__,_| \__,_||_| |_| \__| \__,_||_| |_| |_|
+//                                                     
+//														
+//BY FortyTwocn / 2024
+//QQ:760450201
+onEvent('recipes', event => {
+	event.remove({id: 'appliedenergistics2:smelting/silicon'})
+	event.replaceInput({type: 'appliedenergistics2:inscriber'}, 'appliedenergistics2:silicon', '#forge:silicon')
+	event.replaceInput({type: 'minecraft:crafting_shaped'}, 'appliedenergistics2:silicon', '#forge:silicon')
+	event.replaceInput({type: 'appliedenergistics2:grinder'}, 'appliedenergistics2:fluix_crystal', '#forge:gems/fluix')
+	event.replaceInput({}, 'appliedenergistics2:nether_quartz_dust', '#forge:dusts/quartz')
+//
+//fin
+//
+})
