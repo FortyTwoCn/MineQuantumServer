9160242af933be869d70a90e0f89de1fd5e5a936
FortyTwoCn <112876033+FortyTwoCn@users.noreply.github.com>
2025-01-31 19:43:26 +0800

    Update Appliedenergistics2.js

--- kubejs/server_scripts/Enigmatic/Recipes/Appliedenergistics2.js
+++ kubejs/server_scripts/Enigmatic/Recipes/Appliedenergistics2.js
@@ -17,7 +17,7 @@
 //  \___\_\ \__,_| \__,_||_| |_| \__| \__,_||_| |_| |_|
 //                                                     
 //														
-//BY FortyTwocn / 2024
+//CHANGED BY FortyTwocn / 2024
 //QQ:760450201
 onEvent('recipes', event => {
 	event.remove({id: 'appliedenergistics2:smelting/silicon'})
