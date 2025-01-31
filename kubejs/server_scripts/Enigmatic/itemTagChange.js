7e75a83e124b5a53157faee960772869455dc405
FortyTwoCn <112876033+FortyTwoCn@users.noreply.github.com>
2025-01-31 19:46:07 +0800

    Add files via upload

---
+++ kubejs/server_scripts/Enigmatic/itemTagChange.js
@@ -0,0 +1,86 @@
+//   ____                        _                     
+//  / __ \                      | |                    
+// | |  | | _   _   __ _  _ __  | |_  _   _  _ __ ___  
+// | |  | || | | | / _` || '_ \ | __|| | | || '_ ` _ \ 
+// | |__| || |_| || (_| || | | || |_ | |_| || | | | | |
+//  \___\_\ \__,_| \__,_||_| |_| \__| \__,_||_| |_| |_|
+//                                                     
+//														
+//BY FortyTwocn / 2024
+//QQ:760450201
+onEvent('item.tags', event => {
+//硅
+	event.add('appliedenergistics2:silicon', 'emendatusenigmatica:silicon_gem')
+	event.add('refinedstorage:silicon', 'emendatusenigmatica:silicon_gem')
+	event.add('forge:gems/certus_quartz', 'emendatusenigmatica:charged_certus_quartz_gem')
+//水桶
+   	event.add('forge:buckets/water', 'minecraft:water_bucket')
+//CHARGED QUARTZ ORE
+   	event.remove('forge:ores/certus_quartz', 'appliedenergistics2:charged_quartz_ore')
+	event.add('forge:ores/charged_certus_quartz', 'appliedenergistics2:charged_quartz_ore')
+//QUARTZ ORE
+	event.remove('forge:ores/certus_quartz', 'emendatusenigmatica:certus_quartz_ore')
+//末影粉
+	event.add('appliedenergistics2:dusts/ender', 'emendatusenigmatica:ender_dust')
+//锭
+	event.add('forge:ingots', 'extrabotany:orichalcos')
+	event.add('forge:ingots', 'extrabotany:photonium')
+	event.add('forge:ingots', 'extrabotany:shadowium')
+	event.add('forge:ingots', 'extrabotany:aerialite')
+	event.add('forge:ingots', 'immersiveengineering:ingot_aluminum')
+	event.add('forge:ingots', 'immersiveengineering:ingot_hop_graphite')
+	event.add('forge:ingots', 'occultism:iesnium_ingot')
+	event.add('forge:ingots', 'industrialforegoing:pink_slime_ingot')
+	event.add('forge:ingots', 'twilightforest:knightmetal_ingot')
+	event.add('forge:ingots', 'twilightforest:fiery_ingot')
+	event.add('forge:ingots', 'twilightforest:ironwood_ingot')
+	event.add('forge:ingots', 'pneumaticcraft:ingot_iron_compressed')
+	event.add('forge:ingots', 'mythicbotany:alfsteel_ingot')
+	event.add('forge:ingots', 'mekanism:ingot_refined_glowstone')
+	event.add('forge:ingots', 'mekanism:ingot_refined_obsidian')
+	event.add('forge:ingots', 'lazierae2:fluix_steel_ingot')
+	event.add('forge:ingots', 'lazierae2:fluix_iron_ingot')
+	event.add('forge:ingots', 'kubejs:compressed_steel_ingot')
+	event.add('forge:ingots', 'kubejs:hot_compressed_steel_ingot')
+	event.add('forge:ingots', 'kubejs:superheated_compressed_steel_ingot')
+	event.add('forge:ingots', 'minecraft:gold_ingot')
+	event.add('forge:ingots', 'minecraft:netherite_ingot')
+	event.add('forge:ingots', 'astralsorcery:starmetal_ingot')
+	event.add('forge:ingots', 'botania:manasteel_ingot')
+	event.add('forge:ingots', 'botania:terrasteel_ingot')
+	event.add('forge:ingots', 'botania:elementium_ingot')
+	event.add('forge:ingots', 'botania:gaia_ingot')
+	event.add('forge:ingots', 'cataclysm:ignitium_ingot')
+	event.add('forge:ingots', 'steampowered:cast_iron_ingot')
+	event.add('forge:ingots', 'endless:infinity_ingot')
+	event.add('forge:ingots', 'endless:neutronium_ingot')
+	event.add('forge:ingots', 'endless:crystal_matrix_ingot')
+	event.add('forge:ingots', 'emendatusenigmatica:cast_iron_ingot')
+	event.add('forge:ingots', 'emendatusenigmatica:enderium_ingot')
+	event.add('forge:ingots', 'emendatusenigmatica:lumium_ingot')
+	event.add('forge:ingots', 'emendatusenigmatica:signalum_ingot')
+	event.add('forge:ingots', 'emendatusenigmatica:invar_ingot')
+	event.add('forge:ingots', 'emendatusenigmatica:steel_ingot')
+	event.add('forge:ingots', 'emendatusenigmatica:electrum_ingot')
+	event.add('forge:ingots', 'emendatusenigmatica:constantan_ingot')
+	event.add('forge:ingots', 'emendatusenigmatica:brass_ingot')
+	event.add('forge:ingots', 'emendatusenigmatica:bronze_ingot')
+	event.add('forge:ingots', 'emendatusenigmatica:iridium_ingot')
+	event.add('forge:ingots', 'emendatusenigmatica:cobalt_ingot')
+	event.add('forge:ingots', 'emendatusenigmatica:zinc_ingot')
+	event.add('forge:ingots', 'emendatusenigmatica:tin_ingot')
+	event.add('forge:ingots', 'emendatusenigmatica:osmium_ingot')
+	event.add('forge:ingots', 'emendatusenigmatica:uranium_ingot')
+	event.add('forge:ingots', 'emendatusenigmatica:nickel_ingot')
+	event.add('forge:ingots', 'emendatusenigmatica:lead_ingot')
+	event.add('forge:ingots', 'emendatusenigmatica:silver_ingot')
+	event.add('forge:ingots', 'emendatusenigmatica:aluminum_ingot')
+	event.add('forge:ingots', 'emendatusenigmatica:copper_ingot')
+	event.add('forge:ingots', 'draconicevolution:awakened_draconium_ingot')
+	event.add('forge:ingots', 'draconicevolution:draconium_ingot')
+	event.add('forge:ingots', 'steampowered:steel_ingot')
+	event.add('forge:ingots', 'minecraft:iron_ingot')
+//
+//fin
+//
+})
